9ac7708cd8bde318fc63de32f0f3899306dcd60e
Mitchell Harris <mitchellkharris@gmail.com>
2016-08-30 09:17:12 -0600

    More shortcuts

--- brute-force.js
+++ brute-force.js
@@ -1,20 +1,23 @@
 exports.canFitOnCloud = function (capacity,princess,guards) {
+  guards = guards.sort((x,y) => x == y ? 0 : x < y ? 1 : -1);
   //console.log('Trying',capacity,princess,guards);
   if ( princess > capacity ) return false;
   var all =  princess + guards.reduce((m,x) => m+x,0);
   if (all < capacity) return false;
   if (all == capacity) return true;
-  return combos(0,[],capacity,princess,guards);
+  return combos(0,[],0,capacity,princess,guards);
 }
 
-function check(capacity, princess, guards) {
-  return capacity == princess + guards.reduce((m,x) => m+x,0);
-}
-
-function combos(cursor, sofar, capacity, princess, guards ) {
+function combos(cursor, sofar, sofarWeight, capacity, princess, guards ) {
   if(cursor >= guards.length) {
-    return check(capacity, princess, sofar);
+    return capacity == sofarWeight + princess;
   }
-  return combos(cursor+1, sofar, capacity, princess,guards)
-    || combos(cursor+1, sofar.concat([guards[cursor]]), capacity, princess,guards);
+  return withGuard(guards[cursor],cursor,sofar,sofarWeight,capacity,princess,guards)
+    || withGuard(0,cursor,sofar,sofarWeight,capacity,princess,guards);
+}
+
+function withGuard(guard,cursor,sofar,sofarWeight,capacity,princess,guards) {
+  sofarWeight += guard;
+  if(sofarWeight + princess > capacity) return false;
+  return combos(cursor+1, sofar.concat([guard]), sofarWeight, capacity, princess,guards);
 }
