a70c88a32d9f48a99aa66a0b8b55927f34c3d959
Training #3 <training3@Lajas.local>
2017-04-25 17:34:51 -0600

    Implemented binarySearch method

--- binary-search.js
+++ binary-search.js
@@ -1,5 +1,40 @@
 'use strict';
 
 module.exports = function(needle, haystack) {
-  return undefined;
+	var index = binarySearch(needle, haystack, 0, haystack.length - 1);
+	console.log('index', index);
+	return index;
+}
+
+function binarySearch(needle, haystack, lowerIndex, biggerIndex) {
+	if (lowerIndex > biggerIndex) {
+		return - 1;
+	} else if (lowerIndex === biggerIndex) {
+		if (needle === haystack[lowerIndex]) {
+			return lowerIndex;
+		}
+
+		return -1;
+	}
+
+	var middleIndex = parseInt((biggerIndex - lowerIndex) / 2, 10) + lowerIndex;
+
+	if (needle === haystack[middleIndex]) {
+		return middleIndex;
+	}
+
+	var newLowerIndex = lowerIndex;
+	var newBiggerIndex = biggerIndex;
+
+	if (needle > haystack[middleIndex]) {
+		newLowerIndex = middleIndex + 1;
+	} else {
+		newBiggerIndex = middleIndex - 1;
+
+		if (newBiggerIndex === -1) {
+			newBiggerIndex = 0;
+		}
+	}
+
+	return binarySearch(needle, haystack, newLowerIndex, newBiggerIndex);
 }
